a3f97b83bc276abf29cec862554c3cbbf8cd0669
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-01 17:33:02 +0000

    chore(dags): commenting task

--- dags/dge/carto_rem/tasks.py
+++ dags/dge/carto_rem/tasks.py
@@ -73,11 +73,11 @@ def source_grist() -> None:
         normalisation_process_func=process.normalize_dataframe,
         process_func=process.process_agent_rem_variable,
     )
-    agent_experience_pro = create_grist_etl_task(
-        selecteur="agent_experience_pro",
-        normalisation_process_func=process.normalize_dataframe,
-        process_func=process.process_agent_experience_pro,
-    )
+    # agent_experience_pro = create_grist_etl_task(
+    #     selecteur="agent_experience_pro",
+    #     normalisation_process_func=process.normalize_dataframe,
+    #     process_func=process.process_agent_experience_pro,
+    # )
 
     # ordre des tâches
     chain(
@@ -86,7 +86,7 @@ def source_grist() -> None:
             agent_revalorisation(),
             agent_contrat(),
             agent_rem_variable(),
-            agent_experience_pro(),
+            # agent_experience_pro(),
         ]
     )
 
