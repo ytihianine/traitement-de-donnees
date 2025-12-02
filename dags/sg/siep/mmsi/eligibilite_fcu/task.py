84863258983354b26ee6f0b254743875cb00dd6b
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-02 17:29:38 +0000

    fix(dags): add context to function

--- dags/sg/siep/mmsi/eligibilite_fcu/task.py
+++ dags/sg/siep/mmsi/eligibilite_fcu/task.py
@@ -7,4 +7,5 @@ eligibilite_fcu_to_file = create_action_to_file_etl_task(
     output_selecteur="fcu",
     task_id="eligibilite_fcu_to_file",
     action_func=eligibilite_fcu,
+    use_context=True,
 )
