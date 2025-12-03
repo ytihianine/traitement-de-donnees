227504be3130396c0371ced2deda40a6f2a46d9f
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-03 14:39:26 +0000

    feat(utils): add a generic method to convert grist date columns to standard date

---
+++ utils/control/dates.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def convert_grist_date_to_date(df: pd.DataFrame, columns: list[str]) -> pd.DataFrame:
+    for date_col in columns:
+        df[date_col] = pd.to_datetime(df[date_col], unit="s")
+    return df
