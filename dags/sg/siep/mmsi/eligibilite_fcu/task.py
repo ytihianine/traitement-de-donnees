f7e5f1e931d011e68054848a4dddf64872446f19
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-05 10:09:53 +0000

    feat(dags): splitting task into two tasks in fcu dag

--- dags/sg/siep/mmsi/eligibilite_fcu/task.py
+++ dags/sg/siep/mmsi/eligibilite_fcu/task.py
@@ -1,11 +1,21 @@
-from utils.tasks.etl import create_action_to_file_etl_task
+from utils.tasks.etl import (
+    create_action_to_file_etl_task,
+    create_multi_files_input_etl_task,
+)
 
 from dags.sg.siep.mmsi.eligibilite_fcu.actions import eligibilite_fcu
+from dags.sg.siep.mmsi.eligibilite_fcu import process
 
 
-eligibilite_fcu_to_file = create_action_to_file_etl_task(
+get_eligibilite_fcu = create_action_to_file_etl_task(
     output_selecteur="fcu",
     task_id="eligibilite_fcu_to_file",
     action_func=eligibilite_fcu,
     use_context=True,
+)
+
+process_fcu_result = create_multi_files_input_etl_task(
+    input_selecteurs=["fcu"],
+    output_selecteur="fcu_result",
+    process_func=process.process_result,
 )
