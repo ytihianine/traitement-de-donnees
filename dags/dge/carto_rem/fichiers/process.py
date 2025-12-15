711bdc0c38d27668a21208a4b288c9fb781736b4
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-15 11:01:42 +0000

    fix(dags/dge): refactor carto rem dags - files part

---
+++ dags/dge/carto_rem/fichiers/process.py
@@ -0,0 +1,98 @@
+import pandas as pd
+
+from utils.control.text import convert_str_cols_to_date, normalize_whitespace_columns
+
+
+"""
+    Functions de processing des fichiers sources
+"""
+
+
+def process_agent_elem_rem(df: pd.DataFrame) -> pd.DataFrame:
+    # Clean columns
+    df["matricule_agent"] = df["matricule_agent"].astype("int64")
+
+    txt_cols = [
+        "libelle_element",
+    ]
+    df = normalize_whitespace_columns(df=df, columns=txt_cols)
+
+    # Filter les lignes
+    elems_to_keep = [
+        "TRAITEMENT BRUT",
+        "INDEMNITE DE RESIDENCE",
+        "IND. MENSUELLE TECHNICITE",
+        "I.F.S.E.",
+        "IND. COMPENSATRICE CSG",
+    ]
+    df = df.loc[df["libelle_element"].isin(elems_to_keep)]
+
+    return df
+
+
+def process_agent_info_carriere(df: pd.DataFrame) -> pd.DataFrame:
+    txt_cols = [
+        "dge_perimetre",
+        "nom_usuel",
+        "prenom",
+    ]
+    df = normalize_whitespace_columns(df=df, columns=txt_cols)
+    return df
+
+
+def process_agent_contrat(df: pd.DataFrame) -> pd.DataFrame:
+    # Convert columns
+    date_cols = ["date_debut_contrat_actuel", "date_fin_contrat_actuel"]
+    df = convert_str_cols_to_date(
+        df=df, columns=date_cols, str_date_format="%d/%m/%Y", errors="raise"
+    )
+
+    # Init columns
+    cols_to_init = [
+        "date_premier_contrat_mef",
+        "duree_contrat_en_cours_dge",
+        "duree_cumulee_contrats_tout_contrat_mef",
+        "date_de_cdisation",
+        "duree_en_jours_si_coupure_de_contrat",
+        "date_entree_dge",
+        "duree_contrat_en_cours",
+        "fonction_dge_libelle_court",
+        "fonction_dge_libelle_long",
+    ]
+    df[cols_to_init] = pd.NA
+
+    return df
+
+
+"""
+    Functions de processing des fichiers finaux
+"""
+
+
+def process_agent(df_info_car: pd.DataFrame) -> pd.DataFrame:
+    # Keep only needed columns
+    cols_to_keep = [
+        "matricule_agent",
+        "nom_usuel",
+        "prenom",
+        "genre",
+        "age",
+    ]
+    df = df_info_car.loc[:, cols_to_keep]
+
+    return df
+
+
+def process_agent_carriere(df_info_car: pd.DataFrame) -> pd.DataFrame:
+    # Keep only needed columns
+    cols_to_keep = [
+        "matricule_agent",
+        "qualite_statutaire",
+        "corps",
+        "grade",
+        "echelon",
+        "indice_majore",
+    ]
+    df = df_info_car.loc[:, cols_to_keep]
+
+    return df
---
+++ dags/dge/carto_rem/fichiers/tasks.py
@@ -0,0 +1,67 @@
+from airflow.decorators import task_group
+from airflow.models.baseoperator import chain
+
+from utils.tasks.validation import create_validate_params_task
+from utils.config.types import ALL_PARAM_PATHS
+from utils.tasks.etl import (
+    create_file_etl_task,
+    create_multi_files_input_etl_task,
+)
+
+from dags.dge.carto_rem.fichiers import process
+
+validate_params = create_validate_params_task(
+    required_paths=ALL_PARAM_PATHS,
+    require_truthy=None,
+    task_id="validate_dag_params",
+)
+
+
+@task_group
+def source_files() -> None:
+    agent_elem_rem = create_file_etl_task(
+        selecteur="agent_elem_rem",
+        process_func=process.process_agent_elem_rem,
+        add_import_date=False,
+        add_snapshot_id=False,
+    )
+    agent_info_carriere = create_file_etl_task(
+        selecteur="agent_info_carriere",
+        process_func=process.process_agent_info_carriere,
+        add_import_date=False,
+        add_snapshot_id=False,
+    )
+    agent_contrat = create_file_etl_task(
+        selecteur="agent_contrat",
+        process_func=process.process_agent_contrat,
+        add_import_date=False,
+        add_snapshot_id=False,
+    )
+
+    # ordre des tâches
+    chain([agent_elem_rem(), agent_info_carriere(), agent_contrat()])
+
+
+@task_group
+def output_files() -> None:
+    agent = create_multi_files_input_etl_task(
+        output_selecteur="agent",
+        input_selecteurs=[
+            "agent_info_carriere",
+        ],
+        process_func=process.process_agent,
+        add_import_date=False,
+        add_snapshot_id=False,
+    )
+    agent_carriere = create_multi_files_input_etl_task(
+        output_selecteur="agent_carriere",
+        input_selecteurs=[
+            "agent_info_carriere",
+        ],
+        process_func=process.process_agent_carriere,
+        add_import_date=False,
+        add_snapshot_id=False,
+    )
+
+    # ordre des tâches
+    chain([agent(), agent_carriere()])
