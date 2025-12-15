9f6fc51bf194a52baabf6d736366fd738fa29276
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-15 12:18:31 +0000

    fix(dags/dge): update schema name

--- dags/dge/carto_rem/fichiers/dags.py
+++ dags/dge/carto_rem/fichiers/dags.py
@@ -45,7 +45,7 @@ LINK_DOC_DATA = "To define"  # noqa
     default_args=create_default_args(retries=0),
     params=create_dag_params(
         nom_projet=nom_projet,
-        prod_schema="new_cartographie_remuneration",
+        prod_schema="cartographie_remuneration",
         lien_pipeline=LINK_DOC_PIPELINE,
         lien_donnees=LINK_DOC_DATA,
         mail_enable=False,
