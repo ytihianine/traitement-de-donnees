495a3d4db3a2898d11e06fff1c6f74b93e46b54b
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-17 13:52:04 +0000

    fix(dags/commun): update tasks id

--- dags/commun/code_geographique/tasks.py
+++ dags/commun/code_geographique/tasks.py
@@ -11,17 +11,17 @@ def code_geographique() -> None:
     communes = create_action_to_file_etl_task(
         output_selecteur="communes",
         action_func=actions.communes,
-        task_id="communes_to_file",
+        task_id="communes",
     )
     departements = create_action_to_file_etl_task(
         output_selecteur="departements",
         action_func=actions.departements,
-        task_id="departements_to_file",
+        task_id="departements",
     )
     regions = create_action_to_file_etl_task(
         output_selecteur="regions",
         action_func=actions.regions,
-        task_id="regions_to_file",
+        task_id="regions",
     )
     chain(
         communes(),
@@ -35,12 +35,12 @@ def geojson() -> None:
     departements_geojson = create_action_to_file_etl_task(
         output_selecteur="departements_geojson",
         action_func=actions.departement_geojson,
-        task_id="departements_geojson_to_file",
+        task_id="departements_geojson",
     )
     regions_geojson = create_action_to_file_etl_task(
         output_selecteur="regions_geojson",
         action_func=actions.region_geojson,
-        task_id="regions_geojson_to_file",
+        task_id="regions_geojson",
     )
     chain([departements_geojson(), regions_geojson()])
 
@@ -50,11 +50,11 @@ def code_iso() -> None:
     departements_iso = create_action_to_file_etl_task(
         output_selecteur="code_iso_departement",
         action_func=actions.code_iso_departement,
-        task_id="code_iso_departement_to_file",
+        task_id="departements_iso",
     )
     regions_iso = create_action_to_file_etl_task(
         output_selecteur="code_iso_region",
         action_func=actions.code_iso_region,
-        task_id="code_iso_region_to_file",
+        task_id="regions_iso",
     )
     chain([departements_iso(), regions_iso()])
