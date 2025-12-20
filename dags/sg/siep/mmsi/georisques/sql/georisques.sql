d916cdb5a3ef020aa59600a547550a131d5ce2eb
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-20 22:36:27 +0000

    fix(dags/sg/siep): update table definition in georisque script

--- dags/sg/siep/mmsi/georisques/sql/georisques.sql
+++ dags/sg/siep/mmsi/georisques/sql/georisques.sql
@@ -1,11 +1,14 @@
 DROP TABLE IF EXISTS siep.bien_georisque;
 CREATE TABLE IF NOT EXISTS siep.bien_georisque (
-    id BIGSERIAL,
     code_bat_ter BIGINT NOT NULL,
     risque_categorie TEXT,
     risque_libelle TEXT,
     risque_present BOOLEAN,
     statut TEXT,
     statut_code INT,
-    raison TEXT
-);
+    raison TEXT,
+    import_timestamp TIMESTAMP NOT NULL,
+    import_date DATE NOT NULL,
+    snapshot_id TEXT,
+    PRIMARY KEY (snapshot_id, import_timestamp, code_bat_ter, risque_categorie, risque_libelle)
+) PARTITION BY RANGE (import_timestamp) ;
