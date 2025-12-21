58107dbe13b283d05bfae6eed22b3ba66be86987
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-21 00:21:58 +0000

    feat(scripts): update default values in config

--- scripts/handle_partitions/config.py
+++ scripts/handle_partitions/config.py
@@ -5,24 +5,22 @@ from scripts.handle_partitions.commun import Actions
 # Database
 schema = "siep"
 tbl_to_keep = ("conso", "bien_information", "facture")
-action = Actions.DROP
+action = Actions.CREATE
 dry_run = True
 
 # Si Actions.DROP
-drop_from_date = (
-    None  # else datetime.strptime("31/07/2025 00:00:00", "%d/%m/%Y %H:%M:%S")
-)
+drop_from_date = None  # else [datetime.strptime("31/07/2025", "%d/%m/%Y"), ..., datetime.strptime("31/08/2025", "%d/%m/%Y")]
 
 
 # Si Actions.CREATE
-str_date = "31/07/2025 00:00:00"
-create_from_date = datetime.strptime(str_date, "%d/%m/%Y %H:%M:%S")
+str_date = "01/11/2025"
+create_from_date = datetime.strptime(str_date, "%d/%m/%Y")
 to_date = create_from_date + timedelta(days=1)
 print(create_from_date, to_date)
 
 # Si Actions.UPDATE_TIMESTAMP
-curr_import_timestamp = datetime.strptime("2025-12-20 22:55:24", "%Y-%m-%d %H:%M:%S")
-new_import_timestamp = datetime.strptime("2025-07-31 12:00:00", "%Y-%m-%d %H:%M:%S")
+curr_import_timestamp = datetime.strptime("2025-12-20 23:45:22", "%Y-%m-%d %H:%M:%S")
+new_import_timestamp = datetime.strptime("2025-11-01 12:00:00", "%Y-%m-%d %H:%M:%S")
 print(curr_import_timestamp, new_import_timestamp)
 
 # Si Actions.UPDATE_SNAPSHOT
