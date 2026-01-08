f1c2a5701d0e01713e4db607407bb068894693d9
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-01-08 12:48:09 +0000

    fix(dags/transverse): update dag description of liste_packages dag

--- dags/transverse/liste_packages/packages_dags.py
+++ dags/transverse/liste_packages/packages_dags.py
@@ -1,33 +1,30 @@
-from airflow import DAG
+from typing import Literal
+
 from airflow.sdk import dag, task
-from datetime import datetime, timedelta
 
-import pendulum
-
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": pendulum.today(tz="UTC").add(days=-1),
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=1),
-}
+from utils.config.dag_params import create_dag_params, create_default_args
+from utils.config.types import DagStatus
 
 
 # Définition du DAG
 @dag(
-    "liste-des-packages",
+    dag_id="liste-des-packages",
     schedule="@once",
     max_active_runs=1,
     catchup=False,
     tags=["SG", "DSCI", "PRODUCTION", "INFO"],
     description="Liste des packages installés dans l'instance",
-    default_args=default_args,
+    default_args=create_default_args(),
+    params=create_dag_params(
+        nom_projet="liste-des-packages",
+        dag_status=DagStatus.RUN,
+        prod_schema="Aucun",
+        mail_enable=False,
+    ),
 )
-def liste_packages():
+def liste_packages() -> None:
     @task.bash
-    def bash_task():
+    def bash_task() -> Literal["pip freeze"]:
         return "pip freeze"
 
     bash_task()
