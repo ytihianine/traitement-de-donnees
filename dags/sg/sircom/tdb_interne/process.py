fix(dags/sircom): using generic grist dataset normalisation function and removing old functions

dags/sg/sircom/tdb_interne/process.py
@@ -8,21 +8,11 @@ from utils.control.number import is_in_range, is_upper
 from utils.dataframe import tag_last_value_rows
 
 
-def generic_convert_to_float(value: str) -> float:
+def generic_convert_to_float(value: str | None) -> float | None:
     if value:
-        value = round(float(value) / 100, 4)
-        return value
+        result = round(number=float(value) / 100, ndigits=4)
+        return result
     return None
-
-
-def clean_and_normalize_df(df: pd.DataFrame) -> pd.DataFrame:
-    df.columns = map(str.lower, df.columns)
-    # Drop des colonnes que si elles existent
-    df = df.drop(columns=["id", "commentaire_archive"], errors="ignore")
-    df = df.drop(df.filter(regex="^(grist|manual)").columns, axis=1)
-
-    df = df.fillna(np.nan).replace([np.nan], [None])
-    return df
 
 
 def convert_bytes_to_string(df: pd.DataFrame) -> pd.DataFrame: