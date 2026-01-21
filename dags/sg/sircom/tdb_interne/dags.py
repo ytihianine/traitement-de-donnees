74cd810502303b88d9e26a4f2bfadf991ee3c828
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-01-21 11:11:26 +0000

    fix(dags/sg): remove keep_id_col arg in import files to db task

--- dags/sg/sircom/tdb_interne/dags.py
+++ dags/sg/sircom/tdb_interne/dags.py
@@ -65,7 +65,7 @@ def tdb_sircom() -> None:
         ),
         [abonnes_visites(), budget(), enquetes(), metiers(), ressources_humaines()],
         create_tmp_tables(),
-        import_file_to_db.partial(keep_file_id_col=True).expand(
+        import_file_to_db.expand(
             selecteur_config=get_projet_config(nom_projet=nom_projet)
         ),
         copy_tmp_table_to_real_table(),
