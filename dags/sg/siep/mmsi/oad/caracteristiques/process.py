22b5ef8134cf3c2a16e57921cfb64d15fd5dc2f1
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-01-22 11:03:45 +0000

    fix(dags/siep): add cols_to_keep var in oad carac tasks

--- dags/sg/siep/mmsi/oad/caracteristiques/process.py
+++ dags/sg/siep/mmsi/oad/caracteristiques/process.py
@@ -33,6 +33,14 @@ def process_oad_file(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def process_sites(df: pd.DataFrame) -> pd.DataFrame:
+    # Conserver uniquement les colonnes de ce dataset
+    cols_to_keep = [
+        "code_site",
+        "libelle_site",
+        "site_mef_hmef",
+    ]
+    df = df.loc[:, cols_to_keep]
+
     df = (
         df.dropna(subset=["code_site"])
         .assign(libelle_site=df["libelle_site"].str.upper())
@@ -45,6 +53,34 @@ def process_sites(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def process_biens(df: pd.DataFrame) -> pd.DataFrame:
+    # Conserver uniquement les colonnes de ce dataset
+    cols_to_keep = [
+        "categorie_administrative_liste_bat",
+        "categorie_administrative_principale_bat",
+        "code_bat_ter",
+        "code_site",
+        "date_construction_annee_corrigee",
+        "gest_princ_personnalite_juridique",
+        "gest_princ_personnalite_juridique_precision",
+        "gest_princ_personnalite_juridique_simplifiee",
+        "gestion_categorie",
+        "gestion_mono_multi_mef",
+        "gestion_mono_multi_min",
+        "gestionnaire_presents_liste_mef",
+        "gestionnaire_principal_code",
+        "gestionnaire_principal_libelle",
+        "gestionnaire_principal_libelle_abrege",
+        "gestionnaire_principal_libelle_simplifie",
+        "gestionnaire_principal_lien_mef",
+        "gestionnaire_principal_ministere",
+        "gestionnaire_type_simplifie_bat",
+        "groupe_autorisation",
+        "libelle_bat_ter",
+        "periode",
+        "presence_mef_bat",
+    ]
+    df = df.loc[:, cols_to_keep]
+
     df = (
         df.dropna(subset=["code_bat_ter"])
         .assign(libelle_bat_ter=df["libelle_bat_ter"].str.upper())
@@ -55,6 +91,20 @@ def process_biens(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def process_gestionnaires(df: pd.DataFrame) -> pd.DataFrame:
+    # Conserver uniquement les colonnes de ce dataset
+    cols_to_keep = [
+        "code_gestionnaire",
+        "libelle_abrege",
+        "libelle_gestionnaire",
+        "libelle_simplifie",
+        "lien_mef_gestionnaire",
+        "ministere",
+        "personnalite_juridique",
+        "personnalite_juridique_precision",
+        "personnalite_juridique_simplifiee",
+    ]
+    df = df.loc[:, cols_to_keep]
+
     df = (
         df.dropna(subset=["code_gestionnaire"])
         .assign(
@@ -73,6 +123,16 @@ def process_gestionnaires(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def process_biens_gestionnaires(df: pd.DataFrame) -> pd.DataFrame:
+    # Conserver uniquement les colonnes de ce dataset
+    cols_to_keep = [
+        "code_bat_ter",
+        "code_gestionnaire",
+        "indicateur_poste_gest_source",
+        "indicateur_resident_gest_source",
+        "indicateur_sub_gest_source",
+    ]
+    df = df.loc[:, cols_to_keep]
+
     df = df.dropna(subset=["code_bat_ter", "code_gestionnaire"]).convert_dtypes()
     df = df.drop_duplicates(
         subset=["code_bat_ter", "code_gestionnaire"],
@@ -87,6 +147,35 @@ def process_biens_gestionnaires(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def process_biens_occupants(df: pd.DataFrame) -> pd.DataFrame:
+    # Conserver uniquement les colonnes de ce dataset
+    cols_to_keep = [
+        "categorie_administrative",
+        "categorie_administrative_simplifiee",
+        "code_bat_ter",
+        "code_gestionnaire",
+        "coherence_erreur_possible_presence_occupant",
+        "coherence_indicateur_resident_revu_occ",
+        "coherence_indicateur_sub_revu_occ",
+        "comprend_service_ac",
+        "direction_locale_occupante",
+        "direction_locale_occupante_principale",
+        "filtre_spsi_initial",
+        "filtre_spsi_maj",
+        "indicateur_poste_occ",
+        "indicateur_poste_occ_source",
+        "indicateur_resident_occ",
+        "indicateur_resident_occ_source",
+        "indicateur_resident_reconstitue_occ",
+        "indicateur_sub_occ",
+        "indicateur_sub_occ_source",
+        "indicateur_surface_mef_occ",
+        "indicateur_surface_spsi_m_sub_occ_initial",
+        "indicateur_surface_spsi_m_sub_occ_maj",
+        "occupant",
+        "service_occupant",
+    ]
+    df = df.loc[:, cols_to_keep]
+
     df = df.dropna(subset=["code_bat_ter", "code_gestionnaire"]).convert_dtypes()
 
     # Create calculated columns
