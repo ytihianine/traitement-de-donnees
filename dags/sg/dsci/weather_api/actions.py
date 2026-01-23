760ab94db30057c6ae3b97a27bdce9b812ab142d
Moussa SISSOKO <moussa.sissoko@finances.gouv.fr>
2026-01-23 09:33:06 +0000

    fix(dags/dsci): update download with client http params

--- dags/sg/dsci/weather_api/actions.py
+++ dags/sg/dsci/weather_api/actions.py
@@ -1,27 +1,50 @@
-import requests
 from typing import Any
 from datetime import datetime
 
+# Imports infra
+from infra.http_client.factory import create_http_client
+from infra.http_client.config import ClientConfig
+from entities.dags import HttpHandlerType
+#from dags.sg.dsci.weather_api.variables import PROXY, AGENT
+from utils.config.vars import AGENT, PROXY
 
-def get_weather_from_api() -> dict[str, Any] | None:
+
+def get_weather_from_api() -> dict[str, Any]:
+    """
+    Récupère la météo via le Proxy.
+    Config et Client
+    """
+    print("Initialisation du client HTTP...")
+
+    # --- CONFIGURATION ---
+    # args de ma class CltConfig
+    config = ClientConfig(
+        proxy=PROXY,
+        user_agent=AGENT
+    )
+
+    # --- CRÉATION DU CLIENT ---
+    # Type REQUESTS
+    client = create_http_client(client_type=HttpHandlerType.REQUEST, config=config)
+
+    # --- APPEL API ---
     print("Connexion API Open-Meteo...")
-
     url = "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&current_weather=true"
+    response = client.get(url)
 
-    try:
-        response = requests.get(url)
-        data = response.json()
+    # --- Gestion Erreur --
+    if not response.ok:
+        raise Exception(f"Erreur API ({response.status_code}) : Impossible de récupérer la météo.")
 
-        current_weather = data.get("current_weather")
+    # --- TRAITEMENT DATA ---
+    data = response.json()
+    current_weather = data.get("current_weather")
 
-        return {
-            "date": datetime.now().strftime("%Y-%m-%d"),  # today
-            "city": "Paris",
-            "weather": {
-                "temps": current_weather.get("temperature"),
-                "conditions": f"Wind speed: {current_weather.get('windspeed')} km/h",
-            },
-        }
-    except Exception as e:
-        print(f"Erreur de connexion : {e}")
-        return None
+    return {
+        "date": datetime.now().strftime("%Y-%m-%d"),
+        "city": "Paris",
+        "weather": {
+            "temps": current_weather.get("temperature"),
+            "conditions": f"Wind speed: {current_weather.get('windspeed')} km/h",
+        },
+    }
