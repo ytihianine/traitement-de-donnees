6e0e61065874b1233c930a22be536a3f224fd57d
ytihianine <ytihianine.pro@gmail.com>
2026-01-27 00:24:11 +0100

    update projet tasks to use python primitive types instead of dataclass for compatibility issue

--- utils/tasks/projet.py
+++ utils/tasks/projet.py
@@ -1,11 +1,13 @@
 """Configuration task group for retrieving project configuration at runtime."""
 
+from calendar import c
+from typing import Any, Mapping
 from airflow.sdk import chain, task, task_group
+from dataclasses import asdict
 
 from _types.projet import (
     Contact,
     DbInfo,
-    Documentation,
     ProjetS3,
     SelecteurInfo,
     SelecteurS3,
@@ -25,58 +27,64 @@ from utils.config.tasks import (
 @task()
 def get_documentation_task(
     nom_projet: str | None = None, **context
-) -> list[Documentation]:
+) -> list[Mapping[str, Any]]:
     """Task to fetch project documentation at runtime."""
     docs = get_list_documentation(nom_projet=nom_projet, context=context)
-    return docs
+    return [asdict(doc) for doc in docs]
 
 
 @task()
-def get_contact_task(nom_projet: str | None = None, **context) -> list[Contact]:
+def get_contact_task(
+    nom_projet: str | None = None, **context
+) -> list[Mapping[str, Any]]:
     """Task to fetch project contacts at runtime."""
     contacts = get_list_contact(nom_projet=nom_projet, context=context)
-    return contacts
+    return [asdict(contact) for contact in contacts]
 
 
 @task()
 def get_source_fichier_task(
     nom_projet: str | None = None, **context
-) -> list[SourceFichier]:
+) -> list[Mapping[str, Any]]:
     """Task to fetch file source configurations at runtime."""
     sources = get_list_source_fichier(nom_projet=nom_projet, context=context)
-    return sources
+    return [asdict(source) for source in sources]
 
 
 @task()
-def get_db_info_task(nom_projet: str | None = None, **context) -> list[DbInfo]:
+def get_db_info_task(
+    nom_projet: str | None = None, **context
+) -> list[Mapping[str, Any]]:
     """Task to fetch database info at runtime."""
     db_infos = get_list_database_info(nom_projet=nom_projet, context=context)
-    return db_infos
+    return [asdict(db_info) for db_info in db_infos]
 
 
 @task()
-def get_projet_s3_info_task(nom_projet: str | None = None, **context) -> ProjetS3:
+def get_projet_s3_info_task(
+    nom_projet: str | None = None, **context
+) -> Mapping[str, Any]:
     """Task to fetch project S3 configuration at runtime."""
     projet_s3 = get_projet_s3_info(nom_projet=nom_projet, context=context)
-    return projet_s3
+    return asdict(projet_s3)
 
 
 @task()
 def get_selecteur_s3_task(
     nom_projet: str | None = None, **context
-) -> list[SelecteurS3]:
+) -> list[Mapping[str, Any]]:
     """Task to fetch selecteur S3 configurations at runtime."""
     s3_configs = get_projet_selecteur_s3(nom_projet=nom_projet, context=context)
-    return s3_configs
+    return [asdict(s3_config) for s3_config in s3_configs]
 
 
 @task()
 def get_config_selecteur_info(
     nom_projet: str | None = None, **context
-) -> list[SelecteurInfo]:
+) -> list[Mapping[str, Any]]:
     """Task to fetch selecteur S3 configurations at runtime."""
     s3_db_configs = get_list_selector_info(nom_projet=nom_projet, context=context)
-    return s3_db_configs
+    return [asdict(s3_db_conf) for s3_db_conf in s3_db_configs]
 
 
 @task_group()
