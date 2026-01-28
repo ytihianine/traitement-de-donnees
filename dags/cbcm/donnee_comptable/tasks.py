6d9fb71e995e9ef66bb0fd816d1d04827b57f0be
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-01-28 14:11:31 +0000

    fix(dags/cbcm): add snapshot_id to every datasets

--- dags/cbcm/donnee_comptable/tasks.py
+++ dags/cbcm/donnee_comptable/tasks.py
@@ -16,44 +16,36 @@ def source_files() -> None:
         selecteur="demande_achat",
         process_func=process.process_demande_achat,
         read_options={"skiprows": 3},
-        add_snapshot_id=False,
     )
     engagement_juridique = create_file_etl_task(
         selecteur="engagement_juridique",
         process_func=process.process_engagement_juridique,
-        add_snapshot_id=False,
     )
     demande_paiement = create_file_etl_task(
         selecteur="demande_paiement",
         process_func=process.process_demande_paiement,
-        add_snapshot_id=False,
     )
     demande_paiement_flux = create_file_etl_task(
         selecteur="demande_paiement_flux",
         process_func=process.process_demande_paiement_flux,
         read_options={"skiprows": 3},
-        add_snapshot_id=False,
     )
     demande_paiement_sfp = create_file_etl_task(
         selecteur="demande_paiement_sfp",
         process_func=process.process_demande_paiement_sfp,
-        add_snapshot_id=False,
     )
     demande_paiement_carte_achat = create_file_etl_task(
         selecteur="demande_paiement_carte_achat",
         process_func=process.process_demande_paiement_carte_achat,
-        add_snapshot_id=False,
     )
     demande_paiement_journal_pieces = create_file_etl_task(
         selecteur="demande_paiement_journal_pieces",
         process_func=process.process_demande_paiement_journal_pieces,
-        add_snapshot_id=False,
     )
     delai_global_paiement = create_file_etl_task(
         selecteur="delai_global_paiement",
         process_func=process.process_delai_global_paiement,
         read_options={"skiprows": 3},
-        add_snapshot_id=False,
     )
 
     chain(
