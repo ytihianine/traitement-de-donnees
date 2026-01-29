0baca6af17b6ee0568a5e3c8ce686c44aa7265ec
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-01-29 09:45:17 +0000

    feat(dags/verification): update dag_verification to test more code elements

--- dags/dag_standard/dag_verification.py
+++ dags/dag_standard/dag_verification.py
@@ -1,33 +1,22 @@
-from typing import Any
-
-
 from pprint import pprint
 
 from airflow.sdk import dag, task
 from airflow.sdk.bases.operator import chain
 
-# from infra.mails.default_smtp import create_send_mail_callback, MailStatus
 from infra.mails.default_smtp import (
     send_mail,
-    create_send_mail_callback,
+    _callback,
     MailStatus,
     MailMessage,
 )
+from utils.config.dag_params import create_default_args, create_dag_params
+from _types.dags import DBParams, FeatureFlags
+from enums.dags import DagStatus
 
-from utils.tasks.sql import create_projet_snapshot, get_projet_snapshot
+from utils.tasks.sql import get_projet_snapshot
+from utils.tasks.projet import config_projet_group
 
-
-LINK_DOC_PIPELINE = "https://forge.dgfip.finances.rie.gouv.fr/sg/dsci/lt/airflow-demo/-/tree/main/dags/sg/siep/mmsi/consommation_batiment?ref_type=heads"  # noqa
-LINK_DOC_DONNEES = "https://catalogue-des-donnees.lab.incubateur.finances.rie.gouv.fr/app/dataset?datasetId=49"  # noqa
-
-
-default_args: dict[str, Any] = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 0,
-}
+nom_projet = "Configuration des projets"
 
 
 # Définition du DAG
@@ -38,25 +27,20 @@ default_args: dict[str, Any] = {
     max_consecutive_failed_dag_runs=1,
     catchup=False,
     tags=["SG", "Vérification"],
-    description="Dag de vérification.",  # noqa
-    default_args=default_args,
-    params={
-        "nom_projet": "Projet test",
-        "db": {
-            "prod_schema": "dsci",
-            "tmp_schema": "temporaire",
-        },
-        "mail": {
-            "enable": True,
-            "to": ["yanis.tihianine@finances.gouv.fr"],
-            "cc": ["labo-data@finances.gouv.fr"],
-        },
-        "docs": {
-            "lien_pipeline": LINK_DOC_PIPELINE,
-            "lien_donnees": LINK_DOC_DONNEES,
-        },
-    },
-    on_failure_callback=create_send_mail_callback(mail_status=MailStatus.ERROR),
+    description="Dag de vérification.",
+    default_args=create_default_args(),
+    params=create_dag_params(
+        nom_projet=nom_projet,
+        dag_status=DagStatus.RUN,
+        db_params=DBParams(prod_schema="donnee_comptable"),
+        feature_flags=FeatureFlags(
+            db=True,
+            mail=False,
+            s3=False,
+            convert_files=False,
+            download_grist_doc=False,
+        ),
+    ),
 )
 def dag_verification() -> None:
     @task
@@ -82,23 +66,21 @@ def dag_verification() -> None:
 
     @task
     def send_error_mail(**context) -> None:
-        mail_task = create_send_mail_callback(mail_status=MailStatus.ERROR)
-        mail_task(context=context)
+        _callback(context=context, mail_status=MailStatus.ERROR)
 
     @task
     def send_success_mail(**context) -> None:
-        mail_task = create_send_mail_callback(mail_status=MailStatus.SUCCESS)
-        mail_task(context=context)
+        _callback(context=context, mail_status=MailStatus.SUCCESS)
 
     # Ordre des tâches
     chain(
         [
-            create_projet_snapshot(),
             get_projet_snapshot(),
             print_context(),
             send_simple_mail(),
             send_error_mail(),
             send_success_mail(),
+            config_projet_group(nom_projet=nom_projet),
         ],
     )
 
