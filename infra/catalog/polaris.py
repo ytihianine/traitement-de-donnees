bc15b9693ef3a9db3ed09cde038d1f0a8a015631
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-02-18 12:26:00 +0000

    feat(infra/catalog): add polaris API interface

---
+++ infra/catalog/polaris.py
@@ -0,0 +1,166 @@
+from dataclasses import dataclass
+import requests
+
+
+@dataclass
+class PolarisCatalog:
+    url: str
+    realm: str
+
+    def get_root_access_token(self, client_id: str, client_secret: str) -> str:
+        token_endpoint = f"{self.url}/api/catalog/v1/oauth/tokens"
+        payload = {
+            "grant_type": "client_credentials",
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "scope": "PRINCIPAL_ROLE:ALL",
+        }
+        response = requests.post(url=token_endpoint, data=payload, verify=False)
+        response.raise_for_status()
+        token = response.json().get("access_token")
+        if not token:
+            raise ValueError("Failed to obtain access token")
+        return token
+
+    def create_catalog(
+        self, token: str, catalog_name: str, realm: str | None = None
+    ) -> None:
+        realm = realm if realm else self.realm
+        catalog_endpoint = f"{self.url}/api/management/v1/catalogs"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Polaris-Realm": realm,
+        }
+        payload = {
+            "catalog": {
+                "name": catalog_name,
+                "type": "INTERNAL",
+                "properties": {
+                    "default-base-location": "s3://dsci/data_store",
+                },
+                "storageConfigInfo": {
+                    "storageType": "S3",
+                    "allowedLocations": ["s3://dsci/data_store"],
+                    # "s3AccessKeyId": S3_ACCESS_KEY_ID,
+                    # "s3SecretKey": S3_ACCESS_SECRET_KEY,
+                    "endpoint": "https://minio.lab.incubateur.finances.rie.gouv.fr",
+                    "region": "us-east-1",
+                    "pathStyleAccess": True,  # Required for MinIO
+                },
+            }
+        }
+
+        response = requests.post(
+            url=catalog_endpoint, headers=headers, json=payload, verify=False
+        )
+        response.raise_for_status()
+
+    def create_principal(
+        self, token: str, principal_name: str, realm: str | None = None
+    ) -> tuple[str, str]:
+        realm = realm if realm else self.realm
+        principal_endpoint = f"{self.url}/api/management/v1/principals"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Polaris-Realm": realm,
+        }
+        payload = {"principal": {"name": principal_name, "properties": {}}}
+        response = requests.post(
+            url=principal_endpoint, headers=headers, json=payload, verify=False
+        )
+        response.raise_for_status()
+        data = response.json()
+        return data["credentials"]["clientId"], data["credentials"]["clientSecret"]
+
+    def create_principal_role(
+        self, token: str, role_name: str, realm: str | None = None
+    ) -> None:
+        realm = realm if realm else self.realm
+        role_endpoint = f"{self.url}/api/management/v1/principal-roles"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Polaris-Realm": realm,
+        }
+        payload = {"principalRole": {"name": role_name, "properties": {}}}
+        response = requests.post(
+            url=role_endpoint, headers=headers, json=payload, verify=False
+        )
+        response.raise_for_status()
+
+    def create_catalog_role(
+        self, token: str, catalog_name: str, role_name: str, realm: str | None = None
+    ):
+        realm = realm if realm else self.realm
+        role_endpoint = (
+            f"{self.url}/api/management/v1/catalogs/{catalog_name}/catalog-roles"
+        )
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Polaris-Realm": realm,
+        }
+        payload = {"catalogRole": {"name": role_name, "properties": {}}}
+        response = requests.post(
+            url=role_endpoint, headers=headers, json=payload, verify=False
+        )
+        response.raise_for_status()
+
+    def assign_principal_role(
+        self, token: str, principal_name: str, role_name: str, realm: str | None = None
+    ):
+        realm = realm if realm else self.realm
+        assign_endpoint = f"{self.url}/api/management/v1/principals/{principal_name}/principal-roles"  # noqa
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Polaris-Realm": realm,
+        }
+        payload = {"principalRole": {"name": role_name}}
+        response = requests.put(
+            url=assign_endpoint, headers=headers, json=payload, verify=False
+        )
+        response.raise_for_status()
+
+    def assign_catalog_role(
+        self,
+        token: str,
+        principal_role_name: str,
+        catalog_name: str,
+        catalog_role_name: str,
+        realm: str | None = None,
+    ):
+        realm = realm if realm else self.realm
+        assign_endpoint = f"{self.url}/api/management/v1/principal-roles/{principal_role_name}/catalog-roles/{catalog_name}"  # noqa
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Polaris-Realm": realm,
+        }
+        payload = {"catalogRole": {"name": catalog_role_name}}
+        response = requests.put(
+            url=assign_endpoint, headers=headers, json=payload, verify=False
+        )
+        response.raise_for_status()
+
+    def grant_catalog_privileges(
+        self,
+        token: str,
+        catalog_name: str,
+        catalog_role_name: str,
+        realm: str | None = None,
+    ) -> None:
+        realm = realm if realm else self.realm
+        grant_endpoint = f"{self.url}/api/management/v1/catalogs/{catalog_name}/catalog-roles/{catalog_role_name}/grants"  # noqa
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+            "Polaris-Realm": realm,
+        }
+        payload = {"type": "catalog", "privilege": "CATALOG_MANAGE_CONTENT"}
+        response = requests.put(
+            url=grant_endpoint, headers=headers, json=payload, verify=False
+        )
+        response.raise_for_status()
