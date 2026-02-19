c41f6dadb5d45a57fba144c749cb6dfe688a1473
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-02-19 12:31:41 +0000

    fix(infra/catalog): update write to table iceberg function

--- infra/catalog/iceberg.py
+++ infra/catalog/iceberg.py
@@ -29,12 +29,15 @@ class IcebergCatalog:
         logging.info(msg=f"Creating namespace with name: {namespace}")
         self.catalog.create_namespace_if_not_exists(namespace=namespace)
 
-    def create_table(self, table_name: str, df: pd.DataFrame) -> Table:
+    def create_table(
+        self, table_name: str, df: pd.DataFrame, location: str | None = None
+    ) -> Table:
         # Générer le schéma à partir de la structure du DataFrame
         logging.info(msg=f"Creating table with name: {table_name}")
         table = self.catalog.create_table_if_not_exists(
             identifier=table_name,
             schema=self._get_schema_from_dataframe(df=df),
+            # location=location
         )
         return table
 
@@ -47,16 +50,18 @@ class IcebergCatalog:
             update.union_by_name(new_schema)
         return table
 
-    def write_table(self, table_name: str, df: pd.DataFrame) -> None:
+    def write_table(
+        self, table_name: str, df: pd.DataFrame, overwrite: bool = False
+    ) -> None:
         # Logic to write data to a table in the Iceberg catalog
-        self.update_table(table_name=table_name, df=df)
+        table = self.update_table(table_name=table_name, df=df)
 
         logging.info(msg=f"Writing to table with name: {table_name}")
-        df.to_iceberg(
-            table_name,
-            catalog_name=self.name,
-            catalog_properties=self.catalog.properties,
-        )
+        pa_data = pa.Table.from_pandas(df, preserve_index=False)
+        if overwrite:
+            table.overwrite(df=pa_data)
+        else:
+            table.append(df=pa_data)
         return
 
     def read_table(self, table_name: str) -> pd.DataFrame:
