fdc8be5d199858cb086d57a76fc825551503bd48
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-02-23 13:43:00 +0000

    fix(utils/dates): use second as timestamp precision

--- utils/control/dates.py
+++ utils/control/dates.py
@@ -3,5 +3,7 @@ import pandas as pd
 
 def convert_grist_date_to_date(df: pd.DataFrame, columns: list[str]) -> pd.DataFrame:
     for date_col in columns:
-        df[date_col] = pd.to_datetime(df[date_col], unit="s")
+        df[date_col] = pd.to_datetime(df[date_col], unit="s").astype(
+            dtype="datetime64[s]"
+        )
     return df
