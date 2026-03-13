cb352414e40e6d8b7f4d377caaa9e575e0f83fee
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-13 16:30:51 +0000

    chore(script/partitions): update config file

--- scripts/handle_partitions/config.py
+++ scripts/handle_partitions/config.py
@@ -6,13 +6,15 @@ from scripts.handle_partitions.commun import Actions
 
 # Database
 schema = "siep"
-tbl_to_keep = ("conso", "bien_information", "facture")
-action = Actions.CREATE
-dry_run = True
+tbl_to_keep = ("bien",)
+action = Actions.UPDATE_SNAPSHOT
+dry_run = False
 
 # Si Actions.DROP
 drop_from_date = None
-# else [
+# else
+# drop_from_date = [
+#     datetime.strptime("31/07/2025", "%d/%m/%Y"),
 #   datetime.strptime("31/07/2025", "%d/%m/%Y"),
 #   ...,
 #   datetime.strptime("31/08/2025", "%d/%m/%Y")
@@ -27,13 +29,13 @@ custom_logger.info(msg=create_from_date)
 custom_logger.info(msg=to_date)
 
 # Si Actions.UPDATE_TIMESTAMP
-curr_import_timestamp = datetime.strptime("2025-12-20 23:45:22", "%Y-%m-%d %H:%M:%S")
-new_import_timestamp = datetime.strptime("2025-11-01 12:00:00", "%Y-%m-%d %H:%M:%S")
+curr_import_timestamp = datetime.strptime("2026-03-13 16:04:55", "%Y-%m-%d %H:%M:%S")
+new_import_timestamp = datetime.strptime("2025-02-01 12:00:00", "%Y-%m-%d %H:%M:%S")
 custom_logger.info(msg=curr_import_timestamp)
 custom_logger.info(msg=new_import_timestamp)
 
 # Si Actions.UPDATE_SNAPSHOT
-current_snapshot_id = "20251220_22:55:24"
-new_snapshot_id = "20251101_12:00:00"
+current_snapshot_id = "20260313_16:04:55"
+new_snapshot_id = "20250201_12:00:00"
 custom_logger.info(msg=current_snapshot_id)
 custom_logger.info(msg=new_snapshot_id)
