32bcaa38ba0383ee9151caced27c795f7d20eb4a
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-17 12:48:49 +0000

    fix(dags/cong_projet): specify db_conn_id value

--- dags/applications/configuration_projets/config.py
+++ dags/applications/configuration_projets/config.py
@@ -1,18 +1,41 @@
 from _types.projet import SelecteurStorageOptions
+from utils.config.vars import DEFAULT_PG_CONFIG_CONN_ID
 
 selecteur_options = {
-    "service": SelecteurStorageOptions(),
-    "direction": SelecteurStorageOptions(),
+    "service": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "direction": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
     "grist_doc": SelecteurStorageOptions(
         write_to_db=False,
     ),
-    "projet_contact": SelecteurStorageOptions(),
-    "projet_documentation": SelecteurStorageOptions(),
-    "projet_s3": SelecteurStorageOptions(),
-    "projet_selecteur": SelecteurStorageOptions(),
-    "projets": SelecteurStorageOptions(),
-    "selecteur_column_mapping": SelecteurStorageOptions(),
-    "selecteur_database": SelecteurStorageOptions(),
-    "selecteur_s3": SelecteurStorageOptions(),
-    "selecteur_source": SelecteurStorageOptions(),
+    "projet_contact": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "projet_documentation": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "projet_s3": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "projet_selecteur": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "projets": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "selecteur_column_mapping": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "selecteur_database": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "selecteur_s3": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
+    "selecteur_source": SelecteurStorageOptions(
+        db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
+    ),
 }
