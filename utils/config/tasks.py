e61eca5fc8474ad23c9968b48301323236f259d6
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-18 09:40:44 +0000

    fix(utils/tasks): get additionnal columns to match SelecteurStorageInfo class

--- utils/config/tasks.py
+++ utils/config/tasks.py
@@ -226,7 +226,7 @@ def _get_selecteur_storage_info(
     db = create_db_handler(connection_id=DEFAULT_PG_CONFIG_CONN_ID)
 
     query = f"""
-        SELECT cpss3db.projet, cpss3db.selecteur,
+        SELECT cpss3db.projet, cpss3db.selecteur, cpss3db.type_source, cpss3db.id_source,
             cpss3db.bucket, cpss3db.s3_key, COALESCE(cpss3db.filename, 'filename_undefined') AS filename,
             COALESCE(cpss3db.tbl_name, 'tbl_name_undefined') AS tbl_name
         FROM {CONF_SCHEMA}.selecteur_s3_db_vw cpss3db
