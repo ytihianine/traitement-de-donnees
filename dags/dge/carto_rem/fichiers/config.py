853078aec37726b0268083435a676506f1fac442
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-20 14:41:48 +0000

    fix(dags/dge): update selecteur config options

--- dags/dge/carto_rem/fichiers/config.py
+++ dags/dge/carto_rem/fichiers/config.py
@@ -1,1 +1,17 @@
-selecteur_options = {}
+from _types.projet import SelecteurStorageOptions
+
+
+selecteur_options = {
+    "agent_carriere": SelecteurStorageOptions(
+        write_to_s3=False,
+        write_to_s3_with_iceberg=False,
+    ),
+    "agent": SelecteurStorageOptions(
+        write_to_s3=False,
+        write_to_s3_with_iceberg=False,
+    ),
+    "agent_elem_rem": SelecteurStorageOptions(
+        write_to_s3=False,
+        write_to_s3_with_iceberg=False,
+    ),
+}
