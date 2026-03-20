c1649960f1c608fa6c88345582d89bbe67b92429
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-20 14:45:29 +0000

    fix(dags/dge): add selecteur_options in task_group parameters

--- dags/dge/carto_rem/fichiers/dags.py
+++ dags/dge/carto_rem/fichiers/dags.py
@@ -71,8 +71,12 @@ def cartographie_remuneration() -> None:
         validate_dag_parameters(),
         looking_for_files,
         source_files(),
-        import_files_to_iceberg(nom_projet=nom_projet),
-        iceberg_copy_staging_to_prod(nom_projet=nom_projet),
+        import_files_to_iceberg(
+            nom_projet=nom_projet, selecteur_options=selecteur_options
+        ),
+        iceberg_copy_staging_to_prod(
+            nom_projet=nom_projet, selecteur_options=selecteur_options
+        ),
         copy_s3_files(
             selecteur_options=selecteur_options,
         ),
