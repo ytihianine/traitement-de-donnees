2bef8df8058587df7dad2331667270627a3a405c
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-23 13:40:50 +0000

    feat(utils/s3): update method name to read data

--- utils/tasks/s3.py
+++ utils/tasks/s3.py
@@ -218,7 +218,7 @@ def copy_staging_to_prod(selecteur_config: SelecteurConfig) -> None:
     catalog = IcebergCatalog(name="data_store", properties=properties)
 
     # Read staging table
-    df = catalog.read_table(table_name=namespace + "." + tbl_name + "_staging")
+    df = catalog.read_table_as_df(table_name=namespace + "." + tbl_name + "_staging")
 
     # Write prod table
     write_to_s3(
