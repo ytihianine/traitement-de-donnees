7349988a28194039d465fb9adcc95e6d302296a0
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-25 15:55:54 +0000

    fix(dags/cbcm): set load_strategy to FULL_LOAD for all selecteurs

--- dags/cbcm/donnee_comptable/config.py
+++ dags/cbcm/donnee_comptable/config.py
@@ -1,4 +1,5 @@
 from _types.projet import SelecteurStorageOptions
+from enums.database import LoadStrategy
 
 # Default NULL values
 DEFAULT_NULL_CC_CF = "Ind"
@@ -6,29 +7,38 @@ DEFAULT_NULL_CC_CF = "Ind"
 selecteur_options = {
     "delai_global_paiement": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "demande_achat": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "demande_paiement": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "demande_paiement_carte_achat": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "demande_paiement_complet": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "demande_paiement_flux": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "demande_paiement_journal_pieces": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "demande_paiement_sfp": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "engagement_juridique": SelecteurStorageOptions(
         is_partitioned=False,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
 }
