5d4a6ef049832844e2e5ca2a0cedf4ac4c7ee12d
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-25 15:49:37 +0000

    fix(dags/cbcm): set is_partioned to False for all selecteurs

--- dags/cbcm/donnee_comptable/config.py
+++ dags/cbcm/donnee_comptable/config.py
@@ -1,4 +1,34 @@
+from _types.projet import SelecteurStorageOptions
+
 # Default NULL values
 DEFAULT_NULL_CC_CF = "Ind"
 
-selecteur_options = {}
+selecteur_options = {
+    "delai_global_paiement": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+    "demande_achat": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+    "demande_paiement": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+    "demande_paiement_carte_achat": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+    "demande_paiement_complet": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+    "demande_paiement_flux": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+    "demande_paiement_journal_pieces": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+    "demande_paiement_sfp": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+    "engagement_juridique": SelecteurStorageOptions(
+        is_partitioned=False,
+    ),
+}
