858031d7e14292a7ed8ed6b48779f0c317adffef
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-31 08:25:49 +0000

    fix(dags/catalogue): use new config task

--- dags/applications/catalogue/grist/dags.py
+++ dags/applications/catalogue/grist/dags.py
@@ -4,7 +4,7 @@ from airflow.sdk.bases.operator import chain
 from infra.mails.default_smtp import create_send_mail_callback, MailStatus
 from _types.dags import DBParams, FeatureFlags
 from utils.config.dag_params import create_dag_params, create_default_args
-from utils.config.tasks import get_list_selector_info
+from utils.tasks.projet import get_selecteur_config
 from enums.dags import DagStatus
 from utils.tasks.validation import validate_dag_parameters
 from utils.tasks.grist import download_grist_doc_to_s3
@@ -49,6 +49,8 @@ nom_projet = "Catalogue"
 )
 def catalogue() -> None:
     """Task order"""
+    selecteur_configs = get_selecteur_config(selecteur_mapping={})
+
     chain(
         validate_dag_parameters(),
         download_grist_doc_to_s3(
@@ -61,9 +63,7 @@ def catalogue() -> None:
             source_grist(),
         ],
         create_tmp_tables(reset_id_seq=False),
-        import_file_to_db.expand(
-            selecteur_info=get_list_selector_info(nom_projet=nom_projet)
-        ),
+        import_file_to_db.expand(selecteur_config=selecteur_configs),
         copy_tmp_table_to_real_table(),
         copy_s3_files(),
         del_s3_files(),
