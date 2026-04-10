6b8d7fd4f0cec0629ab8859a247bef4b2b1afb5f
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-04-10 12:42:35 +0000

    feat(dags/conf_projet): update la load_strategy

--- dags/applications/configuration_projets/config.py
+++ dags/applications/configuration_projets/config.py
@@ -1,14 +1,17 @@
 from _types.projet import SelecteurStorageOptions
+from enums.database import LoadStrategy
 from utils.config.vars import DEFAULT_PG_CONFIG_CONN_ID
 
 selecteur_options = {
     "service": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "direction": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "grist_doc": SelecteurStorageOptions(
         write_to_db=False,
@@ -16,37 +19,46 @@ selecteur_options = {
     "projet_contact": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "projet_documentation": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "projet_s3": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "projet_selecteur": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "projets": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "selecteur_column_mapping": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "selecteur_database": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "selecteur_s3": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "selecteur_source": SelecteurStorageOptions(
         db_conn_id=DEFAULT_PG_CONFIG_CONN_ID,
         keep_file_id_col=True,
+        load_strategy=LoadStrategy.APPEND,
     ),
 }
