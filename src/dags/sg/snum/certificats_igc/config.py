edc92aba82298edd83e8c20936f59cc29bf8ce59
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-04-20 16:05:20 +0000

    fix(dags/snum): update selecteur_options in igc dag

--- src/dags/sg/snum/certificats_igc/config.py
+++ src/dags/sg/snum/certificats_igc/config.py
@@ -1,1 +1,6 @@
-selecteur_options = {}
+from src._types.projet import SelecteurStorageOptions
+
+selecteur_options = {
+    "agent": SelecteurStorageOptions(write_to_db=False),
+    "certificat": SelecteurStorageOptions(write_to_db=False),
+}
