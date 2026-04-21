be614f4ebdb4b15b54ae95e4e22c3121344aabc6
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-04-21 16:36:49 +0000

    fix(dags/snum): set load_strategy to APPEND in dag igc

--- src/dags/sg/snum/certificats_igc/config.py
+++ src/dags/sg/snum/certificats_igc/config.py
@@ -1,6 +1,14 @@
-from src._types.projet import SelecteurStorageOptions
+from src._types.projet import SelecteurStorageOptions, LoadStrategy
 
 storage_options = {
-    "agent": SelecteurStorageOptions(write_to_db=False),
-    "certificat": SelecteurStorageOptions(write_to_db=False),
+    "agent": SelecteurStorageOptions(
+        write_to_db=False,
+    ),
+    "certificat": SelecteurStorageOptions(
+        write_to_db=False,
+    ),
+    "aip": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND),
+    "historique_certificat": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND),
+    "liste_certificat": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND),
+    "mandataire": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND),
 }
