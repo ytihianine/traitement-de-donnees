748e16dab3a921d26a20a0f357ecabd5736570e6
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-04-24 09:15:57 +0000

    fix(dags/dsci): set load_strategy to FULL_LOAD instead of INCREMENTAL

--- src/dags/sg/dsci/accompagnements_dsci/config.py
+++ src/dags/sg/dsci/accompagnements_dsci/config.py
@@ -3,120 +3,118 @@ from src._enums.database import LoadStrategy
 
 storage_options = {
     "accompagnement_cci_opportunite": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "accompagnement_cci_quest_satisfaction": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "accompagnement_dsci": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL
+        load_strategy=LoadStrategy.FULL_LOAD
     ),
     "accompagnement_dsci_equipe": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "accompagnement_dsci_porteur": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "accompagnement_dsci_typologie": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
-    "accompagnement_mi": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL
-    ),
+    "accompagnement_mi": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
     "accompagnement_mi_satisfaction": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "animateur_externe": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "animateur_fac": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "animateur_fac_certification": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "animateur_fac_certification_valide": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "animateur_interne": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
-    "bilaterale": SelecteurStorageOptions(load_strategy=LoadStrategy.INCREMENTAL),
+    "bilaterale": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
     "bilaterale_remontee": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
-    "charge_agent_cci": SelecteurStorageOptions(load_strategy=LoadStrategy.INCREMENTAL),
-    "correspondant": SelecteurStorageOptions(load_strategy=LoadStrategy.INCREMENTAL),
+    "charge_agent_cci": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
+    "correspondant": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
     "correspondant_competence_particuliere": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "correspondant_connaissance_communaute": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "correspondant_profil": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
-    "effectif_dsci": SelecteurStorageOptions(load_strategy=LoadStrategy.INCREMENTAL),
+    "effectif_dsci": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
     "fac_hors_bercylab_quest_accompagnement": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
     ),
     "fac_hors_bercylab_quest_accompagnement_facilitateurs": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "fac_hors_bercylab_quest_accompagnement_participants": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "fac_hors_bercylab_quest_type_accompagnement": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "formation_codev_quest_inscription": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "formation_fac_envie_suite_quest_satisfaction": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "formation_fac_quest_satisfaction": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "laboratoires_territoriaux": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "passinnov_quest_inscription": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "passinnov_quest_satisfaction": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "pleniere_quest_inscription": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
     "pleniere_quest_satisfaction": SelecteurStorageOptions(
-        load_strategy=LoadStrategy.INCREMENTAL,
+        load_strategy=LoadStrategy.FULL_LOAD,
         tbl_order=5,
     ),
 }
