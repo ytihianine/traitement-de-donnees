d3b7881264dcfb67241f6d8011c012d45dff0bbf
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-04-27 08:56:40 +0000

    fix(dags/siep): update storage_options

--- src/dags/sg/siep/mmsi/oad/config.py
+++ src/dags/sg/siep/mmsi/oad/config.py
@@ -1,1 +1,55 @@
-storage_options = {}
+from src._types.projet import SelecteurStorageOptions
+from src._enums.database import LoadStrategy
+
+storage_options = {
+    "accessibilite": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "accessibilite_detail": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "bacs": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=2),
+    "bails": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=2),
+    "biens": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=1),
+    "biens_gest": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "biens_occupants": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "couts": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=2),
+    "deet_energie_ges": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "effectif": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=2),
+    "etat_de_sante": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "exploitation": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "gestionnaires": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=1
+    ),
+    "localisation": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "note": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=2),
+    "oad_carac": SelecteurStorageOptions(write_to_db=False),
+    "oad_indic": SelecteurStorageOptions(write_to_db=False),
+    "proprietaire": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "reglementation": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "sites": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=1),
+    "strategie": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "surface": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=2),
+    "typologie": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.APPEND, tbl_order=2
+    ),
+    "valeur": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND, tbl_order=2),
+}
