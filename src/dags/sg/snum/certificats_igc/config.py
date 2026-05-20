cd7732e2435f4e5b34b3b83ad54645ed323c5149
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-05-20 12:22:05 +0000

    fix(dags/igc): update config to FULL_LOAD data at first

--- src/dags/sg/snum/certificats_igc/config.py
+++ src/dags/sg/snum/certificats_igc/config.py
@@ -5,7 +5,10 @@ storage_options = {
         write_to_db=False,
     ),
     "certificat": SelecteurStorageOptions(
-        write_to_db=False,
+        load_strategy=LoadStrategy.FULL_LOAD
+    ),
+    "certificat_contact": SelecteurStorageOptions(
+        load_strategy=LoadStrategy.FULL_LOAD
     ),
     "aip": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
     "historique_certificat": SelecteurStorageOptions(
