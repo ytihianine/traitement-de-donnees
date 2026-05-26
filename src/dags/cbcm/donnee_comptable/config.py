5832f7e0b9987275e08c7c018795d072037376ca
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-05-26 12:23:28 +0000

    chore(dags/cbcm): set load_strategy to APPEND

--- src/dags/cbcm/donnee_comptable/config.py
+++ src/dags/cbcm/donnee_comptable/config.py
@@ -7,38 +7,38 @@ DEFAULT_NULL_CC_CF = "Ind"
 storage_options = {
     "delai_global_paiement": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "demande_achat": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "demande_paiement": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "demande_paiement_carte_achat": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "demande_paiement_complet": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "demande_paiement_flux": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "demande_paiement_journal_pieces": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "demande_paiement_sfp": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
     "engagement_juridique": SelecteurStorageOptions(
         is_partitioned=False,
-        load_strategy=LoadStrategy.FULL_LOAD,
+        load_strategy=LoadStrategy.APPEND,
     ),
 }
