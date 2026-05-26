08b9cefd9fdebc325b9af8b066d72d7a4362d86d
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-05-26 17:43:25 +0000

    fix(common_tasks/sql): update snapshot table definition

--- src/common_tasks/sql/versioning.sql
+++ src/common_tasks/sql/versioning.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA IF NOT EXISTS versioning;
 CREATE TABLE IF NOT EXISTS versioning.snapshot (
     id_row GENERATED ALWAYS AS IDENTITY,
     id_projet INTEGER NOT NULL,
-    snapshot_id TEXT,
+    snapshot_id TEXT NOT NULL,
     import_timestamp TIMESTAMP NOT NULL,
     import_date DATE NOT NULL,
     is_dag_completed BOOLEAN NOT NULL DEFAULT FALSE,
