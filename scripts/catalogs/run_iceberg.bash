2a15cf933ba5de1baf8544dabe9fa061769083af
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-05-29 18:11:18 +0000

    fix(script/catalog): ensure it is running using env python and not global python

--- scripts/catalogs/run_iceberg.bash
+++ scripts/catalogs/run_iceberg.bash
@@ -2,9 +2,10 @@
 
 # Load env vars from centralised .env
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PYTHON_ENV="$SCRIPT_DIR/../../env/bin/python3"
 set -a
 source "$SCRIPT_DIR/../.env"
 set +a
 
 # Execute python script
-python3 "$SCRIPT_DIR/iceberg.py"
+"$PYTHON_ENV" "$SCRIPT_DIR/iceberg.py"
