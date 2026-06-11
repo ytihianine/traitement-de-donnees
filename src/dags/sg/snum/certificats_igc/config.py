feff5582e6333f68c8d157c946f1e4281e2a1a67
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-06-11 16:24:34 +0000

    update agent storage options

--- src/dags/sg/snum/certificats_igc/config.py
+++ src/dags/sg/snum/certificats_igc/config.py
@@ -1,9 +1,7 @@
 from src._types.projet import SelecteurStorageOptions, LoadStrategy
 
 storage_options = {
-    "agent": SelecteurStorageOptions(
-        write_to_db=False,
-    ),
+    "agent": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
     "certificat": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
     "mandataire": SelecteurStorageOptions(load_strategy=LoadStrategy.FULL_LOAD),
 }
