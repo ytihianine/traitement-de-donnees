b086678cbabc9b234afd5f254cb627730237970e
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-06-24 11:09:10 +0000

    feat(scripts): update load_config script

--- scripts/load_config/load_config_projets.py
+++ scripts/load_config/load_config_projets.py
@@ -1,23 +1,23 @@
+import os
+from pathlib import Path
 import sqlite3
 from datetime import datetime
 import pandas as pd
 import numpy as np
-
+import json
 import psycopg2
 from psycopg2.extensions import register_adapter, AsIs
 from psycopg2.extras import execute_values
 
 from src.utils.process.structures import normalize_grist_dataframe
 from src.utils.logs import df_info
-from scripts.settings import get_settings
-
 from src.dags.applications.configuration_projets import process
 
+# Enregistrer l'adaptateur pour les entiers numpy
 register_adapter(typ=np.int64, callable=AsIs)
 
-
-# Tbl order is important due to tbl dependancy !
-tbl_ordered = [
+# Ordre des tables (dépendances)
+TBL_ORDERED = [
     {"tbl_name": "ref_direction", "process_func": process.process_direction},
     {"tbl_name": "ref_service", "process_func": process.process_service},
     {"tbl_name": "projet", "process_func": process.process_projet},
@@ -39,81 +39,126 @@ tbl_ordered = [
         "process_func": process.process_selecteur_database,
     },
 ]
-tbl_names = [action["tbl_name"] for action in tbl_ordered]
-print(tbl_names)
-tbl_names.reverse()
 
-settings = get_settings()
-DRY_RUN = False
-NOW = datetime.now()
-ADD_METADATA = True
-schema = settings.load_config.db_schema
-db_path = settings.load_config.grist_db_path
 
-sqlite_conn = sqlite3.connect(db_path)
-pg_conn = psycopg2.connect(
-    host=settings.db.host,
-    port=settings.db.port,
-    dbname=settings.db.name,
-    user=settings.db.user,
-    password=settings.db.password,
-)
-pg_cur = pg_conn.cursor()
-# for each tbl
+def clear_tables(pg_cur, schema: str, tbl_names: list, dry_run: bool) -> None:
+    """Supprime les données des tables si dry_run est désactivé."""
+    if dry_run:
+        print("DRY_RUN is set to True. Skipping ...")
+        return
 
-if not DRY_RUN:
-    for tbl_desc in tbl_names:
-        # Delete rows
-        drop_query = f"DELETE FROM {schema}.{tbl_desc};"
+    for tbl_name in tbl_names:
+        drop_query = f"DELETE FROM {schema}.{tbl_name};"
         print(drop_query)
         pg_cur.execute(query=drop_query)
 
-for tbl in tbl_ordered:
+
+def process_table(
+    sqlite_conn,
+    tbl_desc: dict,
+    schema: str,
+    pg_cur,
+    now: datetime,
+    add_metadata: bool,
+    dry_run: bool,
+) -> None:
+    """Traite une table : lecture, transformation et insertion."""
     print("\n", "=" * 50)
-    print(f"Start processing table <{tbl["tbl_name"]}>")
-    # read data for sqlite file
+    print(f"Début du traitement de la table <{tbl_desc['tbl_name']}>")
+
+    # Lecture des données depuis SQLite
     df = pd.read_sql_query(
-        sql=f"SELECT * FROM {tbl["tbl_name"].capitalize()}", con=sqlite_conn
+        sql=f"SELECT * FROM {tbl_desc['tbl_name'].capitalize()}",
+        con=sqlite_conn,
     )
     df = normalize_grist_dataframe(df=df)
-    df_info(df=df, df_name=tbl["tbl_name"].capitalize())
-    # apply process function
-    df = tbl["process_func"](df=df)
-    # df = df.drop(df.filter(regex="^(grist|manual)").columns, axis=1)
+    df_info(df=df, df_name=tbl_desc["tbl_name"].capitalize())
+
+    # Application de la fonction de traitement
+    df = tbl_desc["process_func"](df=df)
     df = df.fillna(np.nan).replace([np.nan], [None])
 
-    # Add metadata
-    if ADD_METADATA:
-        df["import_timestamp"] = NOW
-        df["import_date"] = NOW.date()
-        df["snapshot_id"] = NOW.strftime(format="%Y%m%d_%H:%M:%S")
+    # Ajout des métadonnées
+    if add_metadata:
+        df["import_timestamp"] = now
+        df["import_date"] = now.date()
+        df["snapshot_id"] = now.strftime(format="%Y%m%d_%H:%M:%S")
 
-    # Show results
-    # df = df.convert_dtypes()
+    # Affichage des résultats
     print(df.columns)
     print(df.dtypes)
     print(df.isnull().sum())
-    df_info(df=df, df_name=tbl["tbl_name"].capitalize())
+    df_info(df=df, df_name=tbl_desc["tbl_name"].capitalize())
 
-    if not DRY_RUN:
-        # Get tbl columns and order them
-        fetch_query = f"SELECT * FROM {schema}.{tbl["tbl_name"]} LIMIT 0;"
-        pg_cur.execute(query=fetch_query)
-        if pg_cur.description:
-            sorted_cols = sorted(
-                [col.name for col in pg_cur.description if col.name in df.columns]
-            )
-            print(sorted_cols)
+    # Insertion dans PostgreSQL si dry_run est désactivé
+    if dry_run:
+        print()
+        return
 
-            # load data to config db
-            insert_records = df[sorted_cols].to_records(index=False).tolist()
-            insert_query = f"INSERT INTO {schema}.{tbl["tbl_name"]} ({", ".join(sorted_cols)}) VALUES %s"  # noqa
-            print(insert_query)
-            execute_values(cur=pg_cur, sql=insert_query, argslist=insert_records)
-            print(f"End processing table <{tbl["tbl_name"]}>\n")
-        else:
-            print("No results retrieved ...")
+    fetch_query = f"SELECT * FROM {schema}.{tbl_desc['tbl_name']} LIMIT 0;"
+    pg_cur.execute(query=fetch_query)
+    if pg_cur.description:
+        sorted_cols = sorted(
+            [col.name for col in pg_cur.description if col.name in df.columns]
+        )
+        print(sorted_cols)
 
-pg_conn.commit()
-pg_conn.close()
-sqlite_conn.close()
+        insert_records = df[sorted_cols].to_records(index=False).tolist()
+        insert_query = f"""
+            INSERT INTO {schema}.{tbl_desc['tbl_name']}
+            ({", ".join(sorted_cols)})
+            VALUES %s
+        """
+        print(insert_query)
+        execute_values(cur=pg_cur, sql=insert_query, argslist=insert_records)
+        print(f"Fin du traitement de la table <{tbl_desc['tbl_name']}>\n")
+    else:
+        print("Aucun résultat récupéré...")
+
+
+if __name__ == "__main__":
+    dir = os.path.dirname(os.path.realpath(__file__))
+    config_path = Path(dir, "config.json")
+
+    # Load config
+    with open(file=config_path, mode="r") as f:
+        config = json.load(fp=f)
+
+    # Variables
+    dry_run = config["dry_run"]
+    schema = config["db"]["dest_schema"]
+    now = datetime.now()
+    add_metadata = True  # Peut être configuré dans le JSON si nécessaire
+
+    # Init db connections
+    sqlite_conn = sqlite3.connect(config["grist"]["doc_path"])
+    pg_conn = psycopg2.connect(
+        host=config["db"]["host"],
+        port=config["db"]["port"],
+        dbname=config["db"]["name"],
+        user=config["db"]["user"],
+        password=config["db"]["password"],
+    )
+    pg_cur = pg_conn.cursor()
+
+    # Nettoyer les tables si dry_run est désactivé
+    tbl_names = [tbl["tbl_name"] for tbl in TBL_ORDERED]
+    clear_tables(pg_cur=pg_cur, schema=schema, tbl_names=tbl_names, dry_run=dry_run)
+
+    # Traiter chaque table
+    for tbl_desc in TBL_ORDERED:
+        process_table(
+            sqlite_conn=sqlite_conn,
+            tbl_desc=tbl_desc,
+            schema=schema,
+            pg_cur=pg_cur,
+            now=now,
+            add_metadata=add_metadata,
+            dry_run=dry_run,
+        )
+
+    # Valider et fermer les connexions
+    if not dry_run:
+        pg_conn.commit()
+    pg_conn.close()
+    sqlite_conn.close()
