4a3d24a2b476d824daaddf9553a4dfbdca5e5141
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-06-30 08:24:55 +0000

    fix(dags/operat): update source actions

--- src/dags/sg/siep/mmsi/api_operat/actions.py
+++ src/dags/sg/siep/mmsi/api_operat/actions.py
@@ -19,13 +19,19 @@ def build_header() -> dict[str, str]:
     }
 
 
-def get_token(api_client: HttpxClient, url: str, id_structure_assujettie: str) -> str:
+def get_token(
+    api_client: HttpxClient,
+    url: str,
+    id_structure_assujettie: str,
+    cle_tiers: str,
+    cle_utilisateur: str,
+) -> str:
     endpoint = "/api/v1/operat/authentification"
 
     body = {
-        "cleTiers": Variable.get(key="cle_tiers_api_operat"),
+        "cleTiers": cle_tiers,
         "idStructureAssujettie": id_structure_assujettie,
-        "cleUtilisateur": Variable.get(key="cle_utilisateur_api_operat"),
+        "cleUtilisateur": cle_utilisateur,
     }
     headers = build_header()
 
@@ -39,8 +45,10 @@ def get_liste_declarations(api_client: HttpxClient, url: str, token: str) -> dic
     headers = build_header() | {"Authorization": f"Bearer {token}"}
 
     result = api_client.get(endpoint=url + endpoint, headers=headers)
-
-    return result.json()
+    try:
+        return result.json()
+    except Exception:
+        return {"resultat": [{"idConsommation": -1}]}
 
 
 def get_consommation_by_id(
@@ -57,7 +65,14 @@ def get_consommation_by_id(
 # ================
 # Fonctions de processing pour les tâches
 # ================
-def liste_declaration() -> list[dict]:
+def liste_declaration(
+    cle_tiers: str | None = None, cle_utilisateur: str | None = None
+) -> pd.DataFrame:
+    if cle_tiers is None:
+        cle_tiers = str(Variable.get(key="cle_tiers_api_operat"))
+    if cle_utilisateur is None:
+        cle_utilisateur = str(Variable.get(key="cle_utilisateur_api_operat"))
+
     # Http client
     client_config = ClientConfig(user_agent=AGENT, proxy=PROXY)
     httpx_internet_client = HttpxClient(config=client_config)
@@ -72,41 +87,64 @@ def liste_declaration() -> list[dict]:
             api_client=httpx_internet_client,
             url=BASE_URL,
             id_structure_assujettie=id_structure,
+            cle_tiers=cle_tiers,
+            cle_utilisateur=cle_utilisateur,
         )
         lst_declarations = get_liste_declarations(
             api_client=httpx_internet_client, url=BASE_URL, token=token
         )
-        api_result.extend(lst_declarations.get("resultat", []))
+        result_with_structure = [
+            result | {"id_structure": id_structure}
+            for result in lst_declarations.get("resultat", [])
+        ]
+        api_result.extend(result_with_structure)
 
-    logging.info(msg=api_result)
-    return api_result
+    df = pd.DataFrame(data=api_result)
+    return df
 
 
-def consommation_by_id(df: pd.DataFrame) -> list[dict]:
+def consommation_by_id(
+    df: pd.DataFrame, cle_tiers: str | None = None, cle_utilisateur: str | None = None
+) -> pd.DataFrame:
+    if cle_tiers is None:
+        cle_tiers = str(Variable.get(key="cle_tiers_api_operat"))
+    if cle_utilisateur is None:
+        cle_utilisateur = str(Variable.get(key="cle_utilisateur_api_operat"))
+
     # Http client
     client_config = ClientConfig(user_agent=AGENT, proxy=PROXY)
     httpx_internet_client = HttpxClient(config=client_config)
 
-    # Main part
-    api_result = []
+    # Récupérer le token pour chaque structure
+    _token_registry = {}
     for idx, id_structure in enumerate(ID_STRUCTURES):
         logging.info(
-            msg=f"({idx+1}/{len(ID_STRUCTURES)}) Récupération des déclarations pour la structure {id_structure}"
+            msg=f"({idx+1}/{len(ID_STRUCTURES)}) Récupération du token pour la structure {id_structure}"
         )
-        token = get_token(
+        _token_registry[id_structure] = get_token(
             api_client=httpx_internet_client,
             url=BASE_URL,
             id_structure_assujettie=id_structure,
+            cle_tiers=cle_tiers,
+            cle_utilisateur=cle_utilisateur,
         )
 
-        conso_ids = df["id_consommation"]
-        for id_conso in conso_ids:
+    api_result = []
+    for index, row in df.iterrows():
+        logging.info(
+            msg=f"({index+1}/{len(df)}) Récupération des consommations pour la structure {row["id_structure"]} - idConso : {row["idConsommation"]}"
+        )
+        if row["idConsommation"] == -1:
+            logging.warning(msg="Aucune idConsommation pour cette structure")
+        else:
             detail_conso = get_consommation_by_id(
                 api_client=httpx_internet_client,
                 url=BASE_URL,
-                token=token,
-                id_consommation=str(id_conso),
+                token=_token_registry[row["id_structure"]],
+                id_consommation=str(row["idConsommation"]),
             )
-            api_result.extend(detail_conso)
+            # print(detail_conso)
+            api_result.append(detail_conso)
 
-    return api_result
+    df = pd.DataFrame(data=api_result)
+    return df
