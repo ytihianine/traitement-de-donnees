4367d5baaf838bc37cc7d445f388c87088958990
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-09-22 15:27:17 +0000

    move tasks related class to domain model

---
+++ modules/domain/models/tasks.py
@@ -0,0 +1,29 @@
+from collections.abc import Callable
+from dataclasses import dataclass
+from datetime import timedelta
+from typing import Any
+
+from airflow.sdk.definitions._internal.abstractoperator import TaskStateChangeCallback
+
+
+@dataclass(frozen=True)
+class TaskConfig:
+    task_id: str
+    retries: int = 0
+    retry_delay: timedelta | float = 0
+    retry_exponential_backoff: bool = False
+    max_retry_delay: timedelta | float | None = None
+    on_execute_callback: TaskStateChangeCallback | list[TaskStateChangeCallback] | None = None
+    on_failure_callback: TaskStateChangeCallback | list[TaskStateChangeCallback] | None = None
+    on_success_callback: TaskStateChangeCallback | list[TaskStateChangeCallback] | None = None
+    on_retry_callback: TaskStateChangeCallback | list[TaskStateChangeCallback] | None = None
+    on_skipped_callback: TaskStateChangeCallback | list[TaskStateChangeCallback] | None = None
+
+
+@dataclass
+class ETLStep:
+    fn: Callable[..., Any]
+    kwargs: dict[str, Any] | None = None
+    use_context: bool = False
+    read_data: bool = False
+    use_previous_output: bool = False
