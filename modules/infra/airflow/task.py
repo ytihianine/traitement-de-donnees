42c53b083b1114ebb185ffe90317f9164c4e1127
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-09-23 16:56:50 +0000

    init new create_task with domain

--- modules/infra/airflow/task.py
+++ modules/infra/airflow/task.py
@@ -1,7 +1,16 @@
+import logging
+from collections.abc import Callable
 from dataclasses import dataclass
 from datetime import timedelta
 
+from airflow.sdk import XComArg, task
 from airflow.sdk.definitions._internal.abstractoperator import TaskStateChangeCallback
+
+from modules.domain.pipeline.model import ExecutionOptions, PipelineDescriptor
+from modules.infra.airflow.dag import get_project_name
+from modules.infra.file_system.data_writers import FileDatasetWriter
+from modules.infra.file_system.factory import FileHandlerType, FSConfig
+from modules.logs import df_info
 
 
 @dataclass(frozen=True)
@@ -16,3 +25,76 @@ class TaskConfig:
     on_success_callback: TaskStateChangeCallback | list[TaskStateChangeCallback] | None = None
     on_retry_callback: TaskStateChangeCallback | list[TaskStateChangeCallback] | None = None
     on_skipped_callback: TaskStateChangeCallback | list[TaskStateChangeCallback] | None = None
+
+
+def create_task(
+    config: TaskConfig,
+    pipeline: PipelineDescriptor,
+    execution_options: ExecutionOptions,
+) -> Callable[..., XComArg]:
+    """
+    Create a generic Airflow task based on the provided TaskConfig.
+
+    Args:
+        config: Configuration for the task
+        pipeline: Pipeline descriptor
+        execution_options: Execution options for the task
+
+    Returns:
+        An Airflow task that performs the defined ETL steps
+
+    Note:
+        input_selecteurs:
+            - must be provided if any step requires reading data
+            - if there is a single input selector, the DataFrame will be passed as "df".
+            - if multiple input selectors, DataFrames will be passed as "df_{selecteur}"
+    """
+
+    @task(
+        task_id=config.task_id,
+        retries=config.retries,
+        retry_delay=config.retry_delay,
+        retry_exponential_backoff=config.retry_exponential_backoff,
+        max_retry_delay=config.max_retry_delay,
+        on_execute_callback=config.on_execute_callback,
+        on_failure_callback=config.on_failure_callback,
+        on_success_callback=config.on_success_callback,
+        on_retry_callback=config.on_retry_callback,
+        on_skipped_callback=config.on_skipped_callback,
+    )
+    def _task(**context) -> None:
+        """The actual generic task function."""
+        # Hooks & variables
+        nom_projet = get_project_name(context=context)
+
+        # Read data
+
+        # Apply transformations
+        result = None
+        for idx, step in enumerate(pipeline.transformations):
+            logging.info(msg=f"▶ Executing transformation: step_{idx}")
+            logging.info(msg=f"Transformation information: {step}")
+            result = step(result)
+
+        # Export final result - always a DataFrame and the last step output
+        if not execution_options.export_result:
+            return
+
+        if execution_options.add_metadata:
+            projet_metadata = 
+            result = _add_metadata(df=result, nom_projet=nom_projet)
+
+        # Log the final DataFrame information
+        df_info(df=result, df_name=f"{pipeline.output_dataset.name} - df to export")
+
+        # Export result
+        writer = FileDatasetWriter(
+            fs_config=FSConfig(
+                bucket=pipeline.output_dataset.storage.bucket,
+                connection_id=pipeline.output_dataset.storage.s3_conn_id,
+            ),
+            fs_type=FileHandlerType.S3,
+        )
+        writer.write(df=result, dataset=pipeline.output_dataset)
+
+    return _task
