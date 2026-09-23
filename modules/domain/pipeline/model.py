541c072af0e9dfbbe0bb286ad87d654a9bb8d214
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-09-23 16:36:33 +0000

    update pipeline model

--- modules/domain/pipeline/model.py
+++ modules/domain/pipeline/model.py
@@ -1,10 +1,54 @@
 from abc import ABC
 from collections.abc import Callable
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from enum import Enum, auto
+from typing import Any
 
 import pandas as pd
 
+from modules.constants import DEFAULT_PG_DATA_CONN_ID, DEFAULT_S3_CONN_ID
 from modules.domain.dataset.model import Dataset
+
+
+class PartitionTimePeriod(Enum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values) -> str:
+        return name.upper()
+
+    DAY = auto()
+    WEEK = auto()
+    MONTH = auto()
+    YEAR = auto()
+
+
+class LoadStrategy(Enum):
+    """Load strategies for data ingestion."""
+
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values) -> str:
+        return name.upper()
+
+    FULL_LOAD = auto()
+    INCREMENTAL = auto()
+    APPEND = auto()
+
+
+@dataclass(frozen=True)
+class ExecutionOptions:
+    # S3
+    s3_conn_id: str = DEFAULT_S3_CONN_ID
+    write_to_s3: bool = True
+    write_to_s3_with_iceberg: bool = True
+    read_options: dict[str, Any] = field(default_factory=dict)
+    # Database
+    db_conn_id: str = DEFAULT_PG_DATA_CONN_ID
+    write_to_db: bool = True
+    use_prod_schema: bool = True
+    tbl_order: int = 0
+    keep_file_id_col: bool = True
+    is_partitioned: bool = True
+    partition_period: PartitionTimePeriod = PartitionTimePeriod.DAY
+    load_strategy: LoadStrategy = LoadStrategy.APPEND
 
 
 @dataclass(frozen=True)
