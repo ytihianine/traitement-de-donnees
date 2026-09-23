5281ebf88ea8ca571824736161eec3abd187be02
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-09-23 16:08:22 +0000

    update dataset domain model

--- modules/domain/dataset/model.py
+++ modules/domain/dataset/model.py
@@ -1,8 +1,9 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
+from pathlib import Path
 
-from modules.domain.dataset.data_readers import ReaderStrategy
-from modules.domain.dataset.data_writers import WriterStrategy
+import pandas as pd
 
 
 # =================
@@ -32,11 +33,59 @@ class DatasetStorage:
     type_source: TypeSource
     id_source: str | None
 
+    def __post_init__(self) -> None:
+        if not isinstance(self.type_source, TypeSource) and self.type_source is not None:
+            object.__setattr__(self, "type_source", TypeSource(value=self.type_source))
+
+    def get_full_s3_key(
+        self,
+        with_bucket: bool = False,
+        with_tmp_segment: bool = False,
+        use_id_source: bool = False,
+    ) -> str:
+        segments = [self.s3_key]
+        if with_bucket:
+            segments.insert(0, self.bucket)
+        if with_tmp_segment:
+            segments.append("tmp")
+
+        if use_id_source and self.id_source is not None:
+            segments.append(self.id_source)
+        else:
+            segments.append(self.filename)
+
+        return "/".join(segments)
+
+    def get_local_path(self) -> str:
+        if self.filename is None:
+            return str(Path(self.local_dir) / "filename_undefined")
+        return str(Path(self.local_dir) / self.filename)
+
+    def get_iceberg_namespace(self, with_bucket: bool = False) -> str:
+        s3_key = self.get_full_s3_key(with_bucket=with_bucket)
+        namespace_split = s3_key.split(sep=".")[0].split(sep="/")[:-1]
+        return ".".join(namespace_split)
+
 
 @dataclass(frozen=True)
 class Dataset:
     id_projet: int
     name: str
     storage: DatasetStorage
-    reader: ReaderStrategy
-    writers: tuple[WriterStrategy]
+
+
+@dataclass(frozen=True)
+class DatasetReader(ABC):
+
+    @abstractmethod
+    def read(
+        self,
+        dataset: Dataset,
+    ) -> pd.DataFrame: ...
+
+
+@dataclass(frozen=True)
+class DatasetWriter(ABC):
+
+    @abstractmethod
+    def write(self, df: pd.DataFrame, dataset: Dataset) -> None: ...
