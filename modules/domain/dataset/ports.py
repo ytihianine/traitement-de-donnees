4e24a97f61bf0e886cbb2249f8127bf33ec0a10e
ytihianine <ytihianine.pro@gmail.com>
2026-09-25 23:37:49 +0200

    moved back StorageInfo to dataset domain

--- modules/domain/dataset/ports.py
+++ modules/domain/dataset/ports.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 import pandas as pd
 
-from modules.domain.dataset.model import Dataset
+from modules.domain.dataset.model import Dataset, StorageInfo
 
 
 class DatasetReader(ABC):
@@ -18,3 +18,14 @@ class DatasetWriter(ABC):
 
     @abstractmethod
     def write(self, df: pd.DataFrame, dataset: Dataset) -> None: ...
+
+
+class StorageInfoProvider(ABC):
+    @abstractmethod
+    def get_by_dataset(self, nom_projet: str, dataset_name: str) -> StorageInfo: ...
+
+    @abstractmethod
+    def get_by_projet(self, nom_projet: str) -> list[StorageInfo]: ...
+
+    @abstractmethod
+    def get_list_source_fichier(self, nom_projet: str) -> list[str]: ...
