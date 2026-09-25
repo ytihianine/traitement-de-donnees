075be545f402eef815801d324e916066ccd7adf4
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-09-25 12:35:38 +0000

    update projet common tasks

--- modules/infra/airflow/common_tasks/projet.py
+++ modules/infra/airflow/common_tasks/projet.py
@@ -6,9 +6,10 @@ from typing import Any
 
 from airflow.sdk import chain, task, task_group
 
+from modules.domain.dataset.model import Dataset
 from modules.infra.airflow.dag import AirflowDagRepository
+from modules.infra.database.postgres.dataset_repository import PostgresDatasetRepository
 from modules.infra.database.postgres.projet_repository import PostgresProjetRepository
-from modules.types.projet import SelecteurStorageOptions, custom_asdict_factory
 
 
 def _check_nom_projet(nom_projet: str | None, context: dict[str, Any]) -> str:
@@ -38,44 +39,30 @@ def get_contact_task(nom_projet: str | None = None, **context) -> list[Mapping[s
 @task()
 def get_source_fichier_task(nom_projet: str | None = None, **context) -> list[str]:
     """Task to fetch file source configurations at runtime."""
-    projet_repository = PostgresProjetRepository()
     nom_projet = _check_nom_projet(nom_projet=nom_projet, context=context)
-    sources = projet_repository.get_list_source_fichier(nom_projet=nom_projet)
+    dataset_repository = PostgresDatasetRepository()
+    sources = dataset_repository.get_list_source_fichier(nom_projet=nom_projet)
     return sources
 
 
 @task()
-def show_selecteur_config(config: Mapping[str, Any]) -> None:
-    """Task to display selecteur configuration."""
-    print(config)
-
-
-@task()
-def get_selecteur_config(
-    nom_projet: str | None = None, storage_options: Mapping[str, SelecteurStorageOptions] | None = None, **context
-) -> list[dict[str, Any]]:
+def get_projet_datasets(nom_projet: str | None = None, **context) -> list[Dataset]:
     """Task to fetch the project selecteur configurations."""
-    projet_repository = PostgresProjetRepository()
     nom_projet = _check_nom_projet(nom_projet=nom_projet, context=context)
+    dataset_repository = PostgresDatasetRepository()
 
-    selecteurs = projet_repository.get_list_selecteur_storage_info(nom_projet=nom_projet)
-    merged_config = projet_repository.merge_selecteur_config(storage_info=selecteurs, storage_options=storage_options)
+    datasets = dataset_repository.get_list(nom_projet=nom_projet)
 
-    configs = [asdict(obj=sel_config, dict_factory=custom_asdict_factory) for sel_config in merged_config]
-    print(type(configs))
-
-    return configs
+    return datasets
 
 
 @task_group()
-def config_projet_group(
-    nom_projet: str, storage_options: Mapping[str, SelecteurStorageOptions] | None = None, **context
-) -> None:
+def config_projet_group(nom_projet: str | None = None, **context) -> None:
     """
     Groupe de tâches pour récupérer la configuration du projet
 
     Args:
-        nom_projet (Optionnel): Le nom du projet
+        nom_projet (str | None): Le nom du projet
 
     Returns:
         None
@@ -85,9 +72,8 @@ def config_projet_group(
             get_documentation_task(nom_projet=nom_projet, context=context),
             get_contact_task(nom_projet=nom_projet, context=context),
             get_source_fichier_task(nom_projet=nom_projet, context=context),
-            get_selecteur_config(
+            get_projet_datasets(
                 nom_projet=nom_projet,
-                storage_options=storage_options,
             ),
         ]
     )
